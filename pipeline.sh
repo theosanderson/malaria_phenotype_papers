b9421a8937e22e1110392c74cfb39fa42178279e
Theo Sanderson <theo@sndrsn.co.uk>
2022-08-10 17:48:58 +0000

    update

--- pipeline.sh
+++ pipeline.sh
@@ -2,3 +2,4 @@ python search.py -m 5000000 > working/titles.txt
 python title_classification.py
 python abstract_classification.py
 sort working/abstracts_classified.txt | uniq > working/abstracts_classified.sorted.tsv
+python convert.py
