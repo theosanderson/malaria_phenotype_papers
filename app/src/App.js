47459b95262c67d5cf7ae97d5441f043ffcf70f7
Theo Sanderson <theo@sndrsn.co.uk>
2022-08-10 17:42:07 +0000

    add app

---
+++ app/src/App.js
@@ -0,0 +1,92 @@
+import logo from './logo.svg';
+import './App.css';
+import DataTable from 'react-data-table-component-with-filter';
+import {useEffect, useState} from 'react';
+import {ClipLoader} from 'react-spinners'
+
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const url = "./output.json"
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        const data2 = data.sort( (a,b)=>b.pmid > a.pmid)
+        setData(data2);
+        setLoading(false);
+      })
+    }, [])
+
+
+const columns = [
+    {
+   
+        name: 'Title',
+       // selector creates HTML link
+        selector: 'title',
+        // set width to 50%:
+        minWidth: "40%",
+        sortable: true,
+        cell: row => <a href={"https://pubmed.ncbi.nlm.nih.gov/"+row.pmid} style={
+          {
+            color: 'black',
+            textDecoration: 'none',
+          }
+        }>{row.title}</a>
+    },
+    {
+        name: 'PMID',
+        selector: 'pmid',
+        sortable: true,
+        filterable: true
+    },
+    {
+      name: 'Phenotype confidence',
+      selector: 'pheno_conf',
+      sortable: true,
+  },
+    {
+        name: 'Organism',
+        selector: 'organism',
+        sortable: true,
+        filterable: true
+    },
+    {
+        name: 'Organism confidence',
+        selector: 'org_conf',
+        sortable: true,
+        
+    },
+
+];
+
+
+
+    return (<>
+    <h2>Plasmodium phenotype papers</h2>
+    {(loading ? 
+    <ClipLoader /> : 
+    
+        <DataTable
+            columns={columns}
+            data={data}
+            direction="auto"
+           
+            pagination
+            responsive
+            defaultSortFieldId={2}
+            defaultSortAsc = {false}
+           
+            
+        />)}
+        </>
+    );
+
+
+}
+
+export default App;
---
+++ convert.py
@@ -0,0 +1,52 @@
+# read "working/titles.txt" which is tab-separated file with pmid and title, and create dict mapping from pmid to title
+import math
+file = open("working/titles.txt", "rt")
+lines = file.readlines()
+file.close()
+
+pmid_title = {}
+for line in lines:
+    line = line.strip()
+    if line:
+        try:
+            pmid, title = line.split("\t")
+        except ValueError:
+            print("Error:", line)
+            continue
+        pmid_title[pmid] = title
+
+# read "abstracts_classfied.sorted.tsv" which is TSV with pmid, pheno_classification, pheno log_prob, org_classification, org log_prob and iterate through it
+# building a list which will be output to a JSON file
+
+file = open("working/abstracts_classified.sorted.tsv", "rt")
+outputs = []
+for line in file:
+    line = line.strip()
+    if line:
+        pmid, pheno_classification, pheno_log_prob, org_classification, org_log_prob = line.split("\t")
+        if pmid in pmid_title:
+            print(org_classification)
+            if org_classification=="fal":
+                org_classification="P. falciparum"
+            elif org_classification=="berg":
+                org_classification="P. berghei"
+
+            title = pmid_title[pmid]
+            # convert pheno log_prob to percentage probability
+            pheno_log_prob = float(pheno_log_prob)
+            pheno_prob = round(math.exp(pheno_log_prob) * 100, 2)
+            # convert org log_prob to percentage probability
+            org_log_prob = float(org_log_prob)
+            org_prob = round(math.exp(org_log_prob) * 100, 2)
+            if pheno_classification == "phenotype":
+                output = {"title": title, "pmid":int(pmid), "pheno_conf": pheno_prob,"organism":org_classification, "org_conf": org_prob}
+                outputs.append(output)
+file.close()
+
+# write outputs to app/public/output.json
+file = open("app/public/output.json", "wt")
+import json
+# sort outputs by pmid
+outputs = sorted(outputs, key=lambda k: k['pmid'])
+file.write(json.dumps(outputs))
+file.close()
